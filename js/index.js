24db78dd347f96a792fc23250a1a97404dc46b03
Corey Greeneltch <cgreeneltch@gmail.com>
2016-11-05 13:42:21 -0400

    cleanup
    text cleanup

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+
+
 $('document').ready(function() {
-	var options = { videoId: 'aLQeqyOK4lE', start: 3 };
+
+  if (Modernizr.touch) {
+alert('mobile')
+}
+else 
+{
+   
+    	var options = { videoId: 'aLQeqyOK4lE', start: 3 };
 	$('#wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
+}
 });
